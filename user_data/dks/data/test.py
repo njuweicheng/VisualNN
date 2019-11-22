569bee9f2a40980d213adbb9e0d99d021c98d0bc
KSDeng <846335423@qq.com>
2019-11-22 05:44:36 -0800

    modify gitignore

--- user_data/dks/data/test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-dest = 'http://ubuntu:6006'
-#options = webdriver.FireFoxOptions()
-#options.add_argument('--ignore-certificate-errors')
-#browser = webdriver.Firefox(firefox_options=options)
-browser = webdriver.Firefox()
-browser.get(url=dest)
-
